640272d395acd9ef2b64207239ae9ec4645cb926
inam <inamzeynalli01@gmail.com>
2025-03-21 02:04:43 +0400

    123

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,21 +1,4 @@
-const game = {
-  resources: {
-    gold: 250,
-    lumber: 100,
-  },
-  addResource(resource, amount) {
-    if (this.resources.hasOwnProperty(resource)) {
-      this.resources[resource] += amount;
-    } else {
-      console.log("Invalid resource");
-    }
-  }
-};
 
-// Примеры использования:
-game.addResource("gold", 50);  // Увеличит золото на 50
-console.log(game.resources.gold); // 300
-game.addResource("stone", 30); // "Invalid resource"
 
 const gallery = {
   'Mona Lisa': 'Leonardo da Vinci',
